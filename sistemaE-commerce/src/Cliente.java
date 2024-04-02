87d7e007af40119fbb147698a69fc5f2c46f64a7
paulamespindola <paulamespindola>
2024-04-01 22:38:07 -0300

    primeiro commit

---
+++ sistemaE-commerce/src/Cliente.java
@@ -0,0 +1,42 @@
+public class Cliente {
+    private int idCliente;
+    private String nome;
+    private int cpf;
+    private int telefone;
+    private String email;
+
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
